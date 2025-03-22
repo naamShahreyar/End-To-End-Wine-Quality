e090c7ef555faacc7f6096078c16585e556665e7
naamShahreyar <shahreyar1411@gmail.com>
2025-03-22 21:13:45 +0530

    Trainer updated

--- src/WineQuality/components/model_trainer.py
+++ src/WineQuality/components/model_trainer.py
@@ -9,7 +9,7 @@ from WineQuality.entity.config_entity import ModelTrainerConfig
 import pickle
 
 
-model_dict = {'Lasso': Lasso(random_state=42), 'Ridge': Ridge(random_state=42)}
+model_dict = {'Lasso': Lasso, 'Ridge': Ridge}
 
 
 class ModelTrainer:
@@ -21,16 +21,23 @@ class ModelTrainer:
         X = train_data.drop(columns=[self.config.target_column])
         y = train_data[self.config.target_column]
 
-        model = model_dict[self.config.model_name]
+        # Validate model name
+        if self.config.model_name not in model_dict:
+            raise ValueError(f"Unsupported model name: {self.config.model_name}. Choose from {list(model_dict.keys())}")
+        
+        model_cls = model_dict[self.config.model_name]
 
         logger.info(f"Training {self.config.model_name} model")
 
-        grid = GridSearchCV(estimator=model, param_grid=self.config.params_grid.params_grid, cv=5, n_jobs=-1)
+        grid = GridSearchCV(estimator=model_cls(random_state=42), param_grid=self.config.params_grid.params_grid, cv=5, n_jobs=-1)
 
         grid.fit(X, y)
 
         logger.info(f"Best Score: {grid.best_score_}")
         logger.info(f"Best Parameters: {grid.best_params_}")
+
+        best_estimator = model_cls(**grid.best_params_, random_state=42)
+        best_estimator.fit(X, y)
 
         # Define paths
         model_save_path = os.path.join(self.config.model_save_dir, self.config.model_name, f"{self.config.model_name}.pkl")
@@ -40,7 +47,7 @@ class ModelTrainer:
         os.makedirs(os.path.dirname(model_save_path), exist_ok=True)
 
         with open(model_save_path, 'wb') as f:
-            pickle.dump(grid.best_estimator_, f)
+            pickle.dump(best_estimator, f)
 
         with open(best_parameters_save_path, 'w') as f:
             json.dump(grid.best_params_, f)
